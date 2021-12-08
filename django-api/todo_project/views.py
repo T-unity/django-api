7ce18d90db72cb5ee4ed61aeee75ab5dec8b9ef1
T-unity <takadanaritoshi@gmail.com>
2021-12-08 21:41:45 +0900

    コメントアウト削除

--- django-api/todo_project/views.py
+++ django-api/todo_project/views.py
@@ -1,13 +1,5 @@
-# https://docs.djangoproject.com/ja/3.2/ref/request-response/#django.http.HttpResponse
 from django.http import HttpResponse
 
-# viewsの関数はリクエストオブジェクトを受け取って
-# レスポンスオブジェクトをreturnする
-# 引数でリクエストオブジェクトを取得（名前は何でもOK）
 def helloworldfunction(req):
-  # まずDjangoコアのHttpResponseクラスを返す
-  # return HttpResponse('')
-
-  # リクエストオブジェクトを受けとってレスポンスオブジェクトを返却
   resobj = HttpResponse('hello world')
   return resobj
