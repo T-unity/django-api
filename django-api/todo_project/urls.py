e681b964ed88feb4d08fbad3c1beb88f05323080
T-unity <takadanaritoshi@gmail.com>
2021-12-08 23:44:26 +0900

    class based viewとhtmlを使ってページを表示

--- django-api/todo_project/urls.py
+++ django-api/todo_project/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 # 同階層のviewsからhelloworldfunctionをimport
-from .views import helloworldfunction
+from .views import helloworldfunction, HelloWorldClass
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     # プロジェクト内のviews.pyにあるhelloworldfunctionをcall
     path('helloworld/', helloworldfunction),
+    # class based viewでcallするhtmlファイル
+    # クラスビューの場合は、views.pyで作成したクラスに.as_viewをつける必要がある
+    path('helloworld2/', HelloWorldClass.as_view())
 ]
--- django-api/todo_project/views.py
+++ django-api/todo_project/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+# 汎用ビュー
+from django.views.generic import TemplateView
 
+# function based view
 def helloworldfunction(req):
   resobj = HttpResponse('hello world')
   return resobj
+
+# class based view
+# インポートした汎用ビューを使用
+class HelloWorldClass(TemplateView):
+  template_name = 'hello.html'
