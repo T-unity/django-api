ab351a5bb63ce30dc42a95dd78a93b8a5f5fdcd1
T-unity <takadanaritoshi@gmail.com>
2021-12-10 21:54:42 +0900

    テストモデルを作成してマイグレーションファイルの生成とマイグレートを実行

--- api-components/test/models.py
+++ api-components/test/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Human(models.Model):
+  # charfieldは引数にmax_lengthの設定必須
+  name = models.CharField(max_length=20)
+  greeting = models.CharField(max_length=50)
