56dd64cbd31f3b688100110cde4801e164390867
T-unity <takadanaritoshi@gmail.com>
2021-12-14 20:56:21 +0900

    既存のモデルにcreated_atとupdated_atを追加。既存のオブジェクトにはTimezone.nowを指定

--- api-components/test/admin.py
+++ api-components/test/admin.py
@@ -4,4 +4,9 @@ from .models import Human, Tweet
 
 # Humanクラスに対して管理画面から操作できるよう登録
 admin.site.register(Human)
-admin.site.register(Tweet)
+
+class TweetAdmin(admin.ModelAdmin):
+  fields = ('name', 'title', 'content', 'created_at', 'updated_at')
+  readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Tweet, TweetAdmin)
--- api-components/test/models.py
+++ api-components/test/models.py
@@ -4,6 +4,8 @@ class Human(models.Model):
   # charfieldは引数にmax_lengthの設定必須
   name = models.CharField(max_length=20)
   greeting = models.CharField(max_length=50)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     # selfなのでオブジェクト自身が持つ情報の何かをreturnできる
@@ -14,6 +16,8 @@ class Tweet(models.Model):
   name = models.ForeignKey(Human, on_delete=models.CASCADE)
   title = models.CharField(max_length=30)
   content = models.CharField(max_length=200)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return self.title
