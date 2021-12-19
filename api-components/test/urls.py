ed4e3c7904c9854489537133a56c42d58280a00c
T-unity <takadanaritoshi@gmail.com>
2021-12-19 17:35:03 +0900

    使われないルーティングを削除

--- api-components/test/urls.py
+++ api-components/test/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .views import HumanList, HumanListAPI, TweetView, TweetAPIView, TweetListAPI
 
 urlpatterns = [
-    # django's view
-    path('list', HumanList.as_view()),
-    path('tweet-list/', TweetView.as_view()),
     # rest_framework's view
     path('api/human/', HumanListAPI.as_view()),
     path('api/tweet/', TweetListAPI.as_view()),
